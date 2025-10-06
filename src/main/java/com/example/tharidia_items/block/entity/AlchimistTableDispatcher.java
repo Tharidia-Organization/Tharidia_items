d1cde4dda593b99adea4177a25930c96b61163ea
Frenk012 <francescocaccavo75@gmail.com>
2025-10-06 17:25:44 +0200

    cleanup

--- src/main/java/com/example/tharidia_items/block/entity/AlchimistTableDispatcher.java
+++
@@ -1,7 +0,0 @@
-package com.example.tharidia_items.block.entity;
-
-public class AlchimistTableDispatcher {
-    public void playSlideshow(AlchimistTableBlockEntity entity) {
-        entity.triggerAnim("base_controller", "slideshow");
-    }
-}
