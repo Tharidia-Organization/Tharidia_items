2aade7bf568d3dbd243f56af4ccea99a6e7305cd
Luca Bertaggia <luca.bertaggia82@gmail.com>
2026-01-26 17:11:43 +0100

    Refactor jei gui

--- src/main/java/com/THproject/tharidia_things/jei/WasherRecipeCategory.java
+++ src/main/java/com/THproject/tharidia_things/jei/WasherRecipeCategory.java
@@ -6,6 +6,7 @@ import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.gui.drawable.IDrawableAnimated;
+import mezz.jei.api.gui.drawable.IDrawableStatic;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
@@ -14,25 +15,20 @@ import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 
 public class WasherRecipeCategory implements IRecipeCategory<WasherRecipe> {
     public static final RecipeType<WasherRecipe> TYPE = RecipeType.create(TharidiaThings.MODID, "washer",
             WasherRecipe.class);
     private final IDrawable background;
-    protected final IDrawableAnimated arrow;
+    private final IDrawableAnimated arrow;
+    private final IDrawableStatic slot;
     private final IDrawable icon;
 
     public WasherRecipeCategory(IGuiHelper helper) {
-        // Using JEI's built-in texture for a simple furnace-like layout
-        // coordinates 0, 168, 82, 26 corresponds to a 2 slot layout with arrow in
-        // standard vanilla gui texture often used by JEI
-        this.background = helper.createDrawable(
-                ResourceLocation.fromNamespaceAndPath("minecraft", "textures/gui/demo_background.png"), 0, 168, 82, 26);
-
+        this.background = helper.createBlankDrawable(68, 26);
         this.arrow = helper.createAnimatedRecipeArrow(100);
-
+        this.slot = helper.getSlotDrawable();
         this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK,
                 new ItemStack(TharidiaThings.WASHER_BLOCK.get()));
 
@@ -61,7 +57,9 @@ public class WasherRecipeCategory implements IRecipeCategory<WasherRecipe> {
     @Override
     public void draw(WasherRecipe recipe, IRecipeSlotsView recipeSlotView, GuiGraphics guiGraphics, double mouseX,
             double mouseY) {
-        arrow.draw(guiGraphics, 24, 5);
+        arrow.draw(guiGraphics, 22, 5);
+        slot.draw(guiGraphics, -1, 4);
+        slot.draw(guiGraphics, 49, 4);
     }
 
     @Override
