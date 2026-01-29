65971d4f97ae1b08058319dd6cfd641b006d18a7
Luca Bertaggia <luca.bertaggia82@gmail.com>
2026-01-29 19:49:02 +0100

    Added all veins blocks and sediments

--- src/main/java/com/THproject/tharidia_things/block/veins/VeinSediment.java
+++
@@ -1,9 +0,0 @@
-package com.THproject.tharidia_things.block.veins;
-
-import net.minecraft.world.level.block.Block;
-
-public class VeinSediment extends Block {
-    public VeinSediment(Properties properties) {
-        super(properties);
-    }
-}
