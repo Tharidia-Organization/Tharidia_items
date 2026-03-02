482f1956c01ba2e931bafdf4b2d627e24a33af7a
Francesco Caccavo <francescocaccavo75@gmail.com>
2026-03-02 01:35:45 +0100

    Updated Tree model, added grow animation and 4 roots animation, still need 4

--- src/main/java/com/THproject/tharidia_things/block/herbalist/herbalist_tree/HerbalistTreeBlockEntity.java
+++ src/main/java/com/THproject/tharidia_things/block/herbalist/herbalist_tree/HerbalistTreeBlockEntity.java
@@ -1,19 +1,31 @@
 package com.THproject.tharidia_things.block.herbalist.herbalist_tree;
 
 import com.THproject.tharidia_things.TharidiaThings;
+import com.THproject.tharidia_things.block.herbalist.pot.PotBlock;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import software.bernie.geckolib.animatable.GeoBlockEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animation.AnimatableManager.ControllerRegistrar;
+import software.bernie.geckolib.animation.AnimationController;
+import software.bernie.geckolib.animation.PlayState;
+import software.bernie.geckolib.animation.RawAnimation;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 public class HerbalistTreeBlockEntity extends BlockEntity implements GeoBlockEntity {
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
+
+    private static final RawAnimation GROW_ANIM = RawAnimation.begin().thenPlayAndHold("grow");
+    private static final RawAnimation ROOT1_ANIM = RawAnimation.begin().thenPlayAndHold("root");
+    private static final RawAnimation ROOT2_ANIM = RawAnimation.begin().thenPlayAndHold("root2");
+    private static final RawAnimation ROOT3_ANIM = RawAnimation.begin().thenPlayAndHold("root3");
+    private static final RawAnimation ROOT4_ANIM = RawAnimation.begin().thenPlayAndHold("root4");
 
     public HerbalistTreeBlockEntity(BlockPos pos, BlockState state) {
         super(TharidiaThings.HERBALIST_TREE_BLOCK_ENTITY.get(), pos, state);
@@ -29,14 +41,59 @@ public class HerbalistTreeBlockEntity extends BlockEntity implements GeoBlockEnt
         super.loadAdditional(tag, registries);
     }
 
+    public boolean hasPotAtRoot(int rootIndex) {
+        Level level = this.getLevel();
+        if (level == null) return false;
+        BlockPos potPos = getPotPositionForRoot(rootIndex);
+        return level.getBlockState(potPos).getBlock() instanceof PotBlock;
+    }
+
+    public BlockPos getPotPositionForRoot(int rootIndex) {
+        BlockPos pos = this.getBlockPos();
+        Direction forwardDir = Direction.NORTH;
+
+        for (int i = 1; i <= rootIndex; i++) {
+            if (i == rootIndex) {
+                BlockPos newPos = pos.relative(forwardDir, 2);
+                Direction sideDir = (i % 2 != 0) ? forwardDir.getCounterClockWise() : forwardDir.getClockWise();
+                return newPos.relative(sideDir, 1);
+            }
+            if (i % 2 == 0) {
+                forwardDir = forwardDir.getClockWise();
+            }
+        }
+        return pos;
+    }
+
     @Override
     public void registerControllers(ControllerRegistrar controllers) {
-        return;
+        // Tree growth animation - plays once on placement and holds at final frame
+        controllers.add(new AnimationController<>(this, "grow", 0, state -> {
+            if (state.getController().getAnimationState() == AnimationController.State.STOPPED) {
+                state.getController().setAnimation(GROW_ANIM);
+            }
+            return PlayState.CONTINUE;
+        }));
+
+        // Branch animation controllers for roots that have animations
+        addRootController(controllers, 1, ROOT1_ANIM);  // Radice1 -> "root"
+        addRootController(controllers, 3, ROOT3_ANIM);  // Radice3 -> "root3"
+        addRootController(controllers, 4, ROOT4_ANIM);  // Radice4 -> "root4"
+        addRootController(controllers, 8, ROOT2_ANIM);  // Radice8 -> "root2"
+    }
+
+    private void addRootController(ControllerRegistrar controllers, int rootIndex, RawAnimation anim) {
+        controllers.add(new AnimationController<>(this, "root" + rootIndex + "_ctrl", 0, state -> {
+            if (hasPotAtRoot(rootIndex)) {
+                state.getController().setAnimation(anim);
+                return PlayState.CONTINUE;
+            }
+            return PlayState.STOP;
+        }));
     }
 
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return this.cache;
     }
-
 }
--- src/main/java/com/THproject/tharidia_things/client/renderer/HerbalistTreeRenderer.java
+++ src/main/java/com/THproject/tharidia_things/client/renderer/HerbalistTreeRenderer.java
@@ -4,15 +4,11 @@ import org.jetbrains.annotations.Nullable;
 
 import com.THproject.tharidia_things.TharidiaThings;
 import com.THproject.tharidia_things.block.herbalist.herbalist_tree.HerbalistTreeBlockEntity;
-import com.THproject.tharidia_things.block.herbalist.pot.PotBlock;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.Level;
 import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.cache.object.GeoBone;
 import software.bernie.geckolib.model.GeoModel;
@@ -33,7 +29,7 @@ public class HerbalistTreeRenderer extends GeoBlockRenderer<HerbalistTreeBlockEn
 
             @Override
             public ResourceLocation getAnimationResource(HerbalistTreeBlockEntity animatable) {
-                return null;
+                return ResourceLocation.fromNamespaceAndPath(TharidiaThings.MODID, "animations/herbalist_tree.animation.json");
             }
         });
     }
@@ -45,36 +41,9 @@ public class HerbalistTreeRenderer extends GeoBlockRenderer<HerbalistTreeBlockEn
         super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight,
                 packedOverlay, colour);
 
-        Level level = animatable.getLevel();
-        BlockPos pos = animatable.getBlockPos();
-        Direction forwardDir = Direction.NORTH;
-        Direction newDir;
-
         for (int i = 1; i <= 8; i++) {
-            if (i % 2 != 0) {
-                BlockPos newPos = pos.relative(forwardDir, 2);
-                newDir = forwardDir.getCounterClockWise();
-                BlockPos potPos = newPos.relative(newDir, 1);
-
-                setBoneVisible(model, String.format("Radice%d", i),
-                        level.getBlockState(potPos).getBlock() instanceof PotBlock);
-            } else {
-                BlockPos newPos = pos.relative(forwardDir, 2);
-                newDir = forwardDir.getClockWise();
-                BlockPos potPos = newPos.relative(newDir, 1);
-
-                setBoneVisible(model, String.format("Radice%d", i),
-                        level.getBlockState(potPos).getBlock() instanceof PotBlock);
-
-                forwardDir = forwardDir.getClockWise();
-            }
+            setBoneVisible(model, "Radice" + i, animatable.hasPotAtRoot(i));
         }
-    }
-
-    @Override
-    public void render(HerbalistTreeBlockEntity animatable, float partialTick, PoseStack poseStack,
-            MultiBufferSource bufferSource, int packedLight, int packedOverlay) {
-        super.render(animatable, partialTick, poseStack, bufferSource, packedLight, packedOverlay);
     }
 
     private void setBoneVisible(BakedGeoModel model, String boneName, boolean visible) {
