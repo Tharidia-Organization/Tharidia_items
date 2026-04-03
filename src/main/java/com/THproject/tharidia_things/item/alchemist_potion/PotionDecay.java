a4bc859f1cb4f27773388e8cf105377019b7bccd
Luca Bertaggia <luca.bertaggia82@gmail.com>
2026-04-03 03:08:30 +0200

    Refactor PotionDecay logic

--- src/main/java/com/THproject/tharidia_things/item/alchemist_potion/PotionDecay.java
+++ src/main/java/com/THproject/tharidia_things/item/alchemist_potion/PotionDecay.java
@@ -1,18 +1,16 @@
 package com.THproject.tharidia_things.item.alchemist_potion;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.THproject.tharidia_things.TharidiaThings;
 
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.alchemy.PotionContents;
+import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.item.component.DyedItemColor;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingEntityUseItemEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
-import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 
 @EventBusSubscriber(modid = TharidiaThings.MODID)
 public class PotionDecay {
@@ -21,35 +19,31 @@ public class PotionDecay {
         if (event.getEntity().level().isClientSide)
             return;
 
-        ItemStack stack = new ItemStack(AlchemistPotions.BALL_POTION.get());
+        ItemStack stack = new ItemStack(AlchemistPotions.DROP_POTION.get());
         stack.set(DataComponents.DYED_COLOR, new DyedItemColor(0xFF0000, false));
+        stack.set(DataComponents.POTION_CONTENTS, new PotionContents(Potions.REGENERATION));
         stack.set(PotionComponents.CRAFTED_TIME, System.currentTimeMillis());
 
         event.getEntity().getInventory().add(stack);
     }
 
     @SubscribeEvent
-    public static void onPotionDecay(PlayerTickEvent.Post event) {
-        Player player = event.getEntity();
-        if (player.level().isClientSide || player.tickCount % 20 != 0)
+    public static void onUseDecayPotion(LivingEntityUseItemEvent.Tick event) {
+        if (event.getEntity().level().isClientSide)
             return;
 
-        List<ItemStack> potionItems = new ArrayList<>();
-        for (int i = 0; i < player.getInventory().getContainerSize(); i++) {
-            if (AlchemistPotions.isPotion(player.getInventory().getItem(i))) {
-                potionItems.add(player.getInventory().getItem(i));
-            }
+        ItemStack stack = event.getItem();
+        if (!AlchemistPotions.isPotion(stack))
+            return;
+
+        Long time = stack.getOrDefault(PotionComponents.CRAFTED_TIME.get(), -1L);
+        if (time == -1L)
+            return;
+
+        int duration = event.getDuration();
+        if ((System.currentTimeMillis() - time) > 10000L && duration <= 5) {
+            event.setCanceled(true);
+            stack.shrink(1);
         }
-
-        if (potionItems.size() == 0)
-            return;
-
-        potionItems.forEach(potion -> {
-            Long time = potion.getOrDefault(PotionComponents.CRAFTED_TIME.get(), -1L);
-            if (time == -1L)
-                return;
-            if ((System.currentTimeMillis() - time) > 10000L)
-                potion.setCount(0);
-        });
     }
 }
